59c1c20975b4e88043ef3f6f30dd21ab91628294
muscar <muscar@dccd8558-800d-11de-88cc-eb9610995869>
2009-09-02 09:23:16 +0000

    Mostly minor changes: - Fixed a small bug in the reader witch caused the dotted lists to be read in a wring way in certain situations - Added the the Interpreter, Package and Symbol classes - Small architectural changes

---
+++ Gray Common Lisp/Backend/Interpreter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gray_Common_Lisp.Frontend;
+using Gray_Common_Lisp.Runtime;
+using System.IO;
+
+namespace Gray_Common_Lisp.Backend
+{
+	class Interpreter
+	{
+		private Reader reader;
+		internal Package package;
+
+		public Interpreter()
+		{
+			reader = new Reader(this);
+			package = new Package("COMMON-LISP-USER");
+			package.InternSpecial("nil", null);
+			package.Intern("t");
+		}
+
+		public object Eval(string source)
+		{
+			return reader.Read(new StringReader(source));
+		}
+	}
+}
--- Gray Common Lisp/Program.cs
+++ Gray Common Lisp/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using Gray_Common_Lisp.Frontend;
+using Gray_Common_Lisp.Backend;
 
 namespace Gray_Common_Lisp
 {
@@ -11,12 +12,12 @@ namespace Gray_Common_Lisp
 	{
 		static void Main(string[] args)
 		{
-			Reader.Init();
+			var interpreter = new Interpreter();
 
 			while (true)
 			{
 				Console.Write("gcl> ");
-				var obj = Reader.Read(new StringReader(Console.ReadLine()));
+				var obj = interpreter.Eval(Console.ReadLine());
 				Console.WriteLine("{0} ; {1}", obj, obj.GetType());
 			}
 		}
